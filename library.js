023713d151234ac9160d8d04979a6b6bdc9e1ff5
CarbonylGroup <carbonylgp@gmail.com>
2018-09-23 10:14:39 +0800

    clean

--- library.js
+++ library.js
@@ -6,7 +6,6 @@ passportLocal = module.parent.require('passport-local').Strategy,
 plugin = {};
 
 plugin.login = function() {
-    // winston.info('[login] Registering new local login strategy');
     passport.use(new passportLocal({passReqToCallback: true}, plugin.continueLogin));
 };
 
@@ -31,45 +30,22 @@ plugin.continueLogin = function(req, musername, mpassword, next) {
                     if (uid == null) {
                         user.create({
                             username: musername,
-                            password: mpassword,
-                            email: '1@why_are_u_seeing_this.com'
+                            password: mpassword
                         });
                     }
                 });
                 user.getUidByUsername(musername, function(err, muid) {
                     if (muid != null ) {
                         next(null, {
-                            uid: muid,
-                            isAdminOrGlobalMod: true
+                            uid: muid
                         }, '[[success:authentication-successful]]');
                     } else {
                         next(new Error('[[error:invalid-username-or-password]]'));
                     }
                 });
-
             }
         }
     );
-
-
-    // But if the login was unsuccessful, pass an error back, like so:
-    // next(new Error('[[error:invalid-username-or-password]]'));
-
-    /*
-    You'll probably want to add login in this method to determine whether a login
-    refers to an existing user (in which case log in as above), or a new user, in
-    which case you'd want to create the user by calling User.create. For your
-    convenience, this is how you'd create a user:
-
-    var user = module.parent.require('./user');
-
-    user.create({
-    username: 'someuser',
-    email: 'someuser@example.com'
-});
-
-Acceptable values are: username, email, password
-*/
 };
 
 module.exports = plugin;
